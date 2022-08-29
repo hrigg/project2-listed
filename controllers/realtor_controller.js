43324963dfdbf8258f4280030ff48f2ec0c376e1
hrigg <rigg.heather6@gmail.com>
2022-08-29 16:01:56 -0400

    added realtor controller routes

--- controllers/realtor_controller.js
+++ controllers/realtor_controller.js
@@ -22,20 +22,18 @@ router.get('/', async (req, res, next) => {
    }
    // res.send('all realtors')
 });
-//////////////
-// Reviews "new" route - GET request- displays form for creating a new review
-// This route will be considered optional, logically we would not want a whole page for creating a review, instead adding a form on a product show page. 
+
+//NEW ROUTE 
 router.get('/new', (req, res) => {
-    res.send('new review')
+    res.send('new realtor')
 })
 
-// Review "show" route - GET request - display details about one product 
-// http://localhost:4000/reviews/0*****
+//SHOW ROUTE
 
 router.get('/:id/', async (req, res, next) => {
     try{
-        const foundReview= await db.Review.findById(req.params.id).populate('product').exec()
-        res.render('reviews/show.ejs', {review:foundReview})
+        const foundRealtor= await db.Review.findById(req.params.id).populate('house').exec()
+        res.render('realtor/show.ejs', {realtor:foundRealtor})
     }
     catch(err){
         console.log(err)
@@ -44,47 +42,41 @@ router.get('/:id/', async (req, res, next) => {
    // res.send('review detail')
 })
 
-// Reviews "edit" route - GET request - display an edit form for one review
-// http://localhost:4000/reviews/0/edit
+//EDIT
 
 router.get('/:id/edit', async (req,res, next)=>{
     res.send('review edit')
-		// note-we will not be demoing this procedure, but is very similar to 'products'
 })
 
-// Review "create" route - POST request -> request body (new review data)
-
-// http://localhost:4000/reviews/
+//POST ROUTE
 
 router.post('/', async (req, res, next) => {
     try{
-        const newReview= await db.Review.create(req.body)
+        const newRealtor= await db.Realtor.create(req.body)
         //res.send(newReview)
-        res.redirect('/reviews/'+newReview._id)
+        res.redirect('/realtor/'+newRealtor._id)
         //res.redirect(`/products/${newReview.product})
     }
     catch(err){
         console.log(err)
         next()
-        //skip this request and run to next avaialbe (404)
     }
     
     //res.send('review create')
 })
 
-// Reviews "destroy" route - DELETE request - removes data from reviews collection and redirects to index route
 
-// http://localhost:4000/reviews/0/ 
+//DELETE
 
 router.delete('/:id', async (req,res, next)=>{
-    res.send('review delete')
+    res.send('realtor delete')
 })
 
-// Reviews "update" route - PUT request - update the Review document and redirects to Product show route
-// http://localhost:4000/reviews/0/
+
+//PUT EDIT
 
 router.put('/:id', async (req, res, next)=>{
-	res.send('review update')
+	res.send('realtor update')
 })
 
 module.exports = router
