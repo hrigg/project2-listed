d07f0de8d32222e7b914fe2c671d960b549b4ef9
SquatchTang <kyjaba@gmail.com>
2022-08-29 09:29:01 -0500

    controller work

--- models/House.js
+++ models/House.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
 
 const houseSchema = new mongoose.Schema({
-    name: { type: String, required: [true, "name can not be empty"],},
-    beds: { type: Number, required: [true, "beds can not be empty"],},
-    baths: {type: Number, required: [true, "baths can not be empty"],},
+    name: {type: String, required: [true, "name can not be empty"],},
+    beds: {type: Number, min: [0, 'you can not add a negative number'], required: [true, "beds can not be empty"],},
+    baths: {type: Number, min: [0, 'you can not add a negative number'], required: [true, "baths can not be empty"],},
     price: {type: Number, min: [0, 'you can not add a negative number'], required: [true, "price can not be empty"],},
     city: {type: String, required: [true, "city can not be empty"],},
 }, {timestamps: true,
