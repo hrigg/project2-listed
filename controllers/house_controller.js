93451d632d4d328fc7fd68edfff76df97e858afd
SquatchTang <kyjaba@gmail.com>
2022-08-29 10:04:51 -0500

    new.ejs

--- controllers/house_controller.js
+++ controllers/house_controller.js
@@ -24,15 +24,26 @@ router.get("/:houseIndex", async (req, res) => {
   });
 
 router.get('/', (req, res) => {
-    res.render('index.ejs')
-});
+    
+    try{
+
+        const dcHouse= await db.House.find({city: dc})
+        const chicagoHouse= await db.House.find({city: chicago})
+        const dallasHouse= await db.House.find({city: dallas})
+        const context= {dcHouse: dcHouse, chicagoHouse: chicagoHouse, dallasHouse: dallasHouse}
+        res.render("index.ejs", context);
+    
+    } catch(err){
+        console.log(err)
+    }  
+    });
 
 router.delete("/:houseId", async (req, res) => {
   try{
 
     const foundHouse = await db.House.findByIdAndDelete(req.params.houseId)
     console.log(foundHouse)
-    return res.redirect("/houses");
+    return res.redirect("/house");
 
 }catch(err){
     console.log(err)
