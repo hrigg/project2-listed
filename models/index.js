7f6ed947cbcbb1b200819c49cdfe3590d68abc9b
hrigg <rigg.heather6@gmail.com>
2022-09-02 10:08:30 -0400

    heroku test

--- models/index.js
+++ models/index.js
@@ -2,4 +2,3 @@ module.exports = {
     House: require('./House'),
     Realtor: require('./Realtor')
 }
-mongoose.connect( process.env.MONGODB_URI || "YOUR CURRENT LOCALHOST DB CONNECTION STRING HERE" );
